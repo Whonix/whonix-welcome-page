50ba03c3b732972a25532abc46bab97312a90cd8
Patrick Schleizer <adrelanos@riseup.net>
2015-01-05 15:42:40 +0000

    set, export environment variable TOR_HOMEPAGE to set TorBrowser homepage to /usr/share/homepage/whonix-welcome-page/whonix.html https://trac.torproject.org/projects/tor/ticket/13835

---
+++ usr/lib/whonix-welcome-page/env_var.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+## This file is part of Whonix.
+## Copyright (C) 2012 - 2014 Patrick Schleizer <adrelanos@riseup.net>
+## See the file COPYING for copying conditions.
+
+## environment variable to set TorBrowser homepage
+## https://trac.torproject.org/projects/tor/ticket/13835
+TOR_HOMEPAGE="/usr/share/homepage/whonix-welcome-page/whonix.html"
+export TOR_HOMEPAGE
