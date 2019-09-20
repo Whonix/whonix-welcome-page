d79010a43d1f0d027f4807090850d4c92f3bcbce
Patrick Schleizer <adrelanos@riseup.net>
2019-09-20 14:00:20 +0000

    no longer set TOR_DEFAULT_HOMEPAGE variable
    https://forums.whonix.org/t/noscript-with-security-slider-at-safest-permits-around-30-sites/8160

--- usr/lib/whonix-welcome-page/env_var.sh
+++ usr/lib/whonix-welcome-page/env_var.sh
@@ -5,5 +5,7 @@
 
 ## environment variable to set TorBrowser homepage
 ## https://trac.torproject.org/projects/tor/ticket/13835
-TOR_DEFAULT_HOMEPAGE="/usr/share/homepage/whonix-welcome-page/whonix.html"
-export TOR_DEFAULT_HOMEPAGE
+
+## https://forums.whonix.org/t/noscript-with-security-slider-at-safest-permits-around-30-sites/8160
+#TOR_DEFAULT_HOMEPAGE="/usr/share/homepage/whonix-welcome-page/whonix.html"
+#export TOR_DEFAULT_HOMEPAGE
